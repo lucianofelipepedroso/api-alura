16fe3b3bd5fab900f757377d997964dc2ee209fe
Luciano Felipe Pedroso <lucianofelipepedroso@hotmail.com>
2023-03-07 09:15:06 -0300

    Adicionados arquivos via upload

---
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,39 @@
+package med.voll.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedicos;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+	
+	@Autowired
+	private MedicoRepository repository;
+	
+	@PostMapping
+	@Transactional
+	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+		repository.save(new Medico(dados));
+		
+	}
+	
+	@GetMapping
+	public List<DadosListagemMedicos> listar(){
+		return repository.findAll().stream().map(DadosListagemMedicos::new).toList();
+	}
+	
+
+}
