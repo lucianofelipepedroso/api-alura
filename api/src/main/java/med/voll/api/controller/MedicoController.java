acfc20e809fd4b910cd2e9071872b59dfa752b2d
Luciano Felipe Pedroso <lucianofelipepedroso@hotmail.com>
2023-03-08 14:53:02 -0300

    Adicionado update

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,12 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.medico.DadosAtualizacaoMedico;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.Medico;
@@ -28,18 +30,23 @@ public class MedicoController {
 	@Transactional
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 		repository.save(new Medico(dados));
-		
+
+	}
+
+	@PutMapping
+	@Transactional
+	public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+		var medico  = repository.getReferenceById(dados.id());
+		medico.atualizarInformacoes(dados);
 	}
 
 	@GetMapping
-	public Page<DadosListagemMedicos> listar (@PageableDefault(size = 10, sort = {"nome"})Pageable paginacao){
+	public Page<DadosListagemMedicos> listar(@PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
 		return repository.findAll(paginacao).map(DadosListagemMedicos::new);
 	}
-	
-	
-	//public List<DadosListagemMedicos> listar(){
-		//return repository.findAll().stream().map(DadosListagemMedicos::new).toList();
-	//}
-	
+
+	// public List<DadosListagemMedicos> listar(){
+	// return repository.findAll().stream().map(DadosListagemMedicos::new).toList();
+	// }
 
 }
